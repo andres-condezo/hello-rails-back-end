c72acb20d62a6522dfc140c78410a2451b126022
Roberto Andres Condezo Monge <ra.condezomonge@gmail.com>
2022-08-11 10:24:24 -0500

    Feat[config/]: [Update route files]

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1, defaults: { format: 'json' } do
+      get 'greetings', to: 'greetings#index'
+    end
+  end
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
